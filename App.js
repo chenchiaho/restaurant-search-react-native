573f6c06e48c32c051335a795690d812a76b3bbc
Kevin La <chenchiaho@gmail.com>
2023-04-25 17:47:30 +0800

    created CategoryItem component

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Header from './src/components/Header';
 import Search from './src/components/Search';
+import CategoryItem from './src/components/CategoryItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header/>
       <Search/>
+      
+      <CategoryItem name="Burger" imageUrl={require('./src/assets/images/burger.png')}/>
+      <CategoryItem name="Pizza" imageUrl={require('./src/assets/images/pizza.png')}/>
+      <CategoryItem name="Dessert" imageUrl={require('./src/assets/images/dessert.png')}/>
       <StatusBar/>
       
     </View>
