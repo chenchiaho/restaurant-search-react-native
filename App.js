d0b81183a12649564e87c208cc8cafa7ce90a493
Kevin La <chenchiaho@gmail.com>
2023-05-03 09:18:06 +0800

    implemented flatlist

--- App.js
+++ App.js
@@ -1,18 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import Header from './src/components/Header';
 import Search from './src/components/Search';
 import CategoryItem from './src/components/CategoryItem';
 
 export default function App() {
+
+    const commonCategories = [
+      {
+        name: "Burger",
+        imageUrl: require("./src/assets/images/burger.png")
+      },
+      {
+        name: "Dessert",
+        imageUrl: require("./src/assets/images/cake.png")
+      },
+      {
+        name: "Drink",
+        imageUrl: require("./src/assets/images/smoothies.png")
+      },
+      {
+        name: "Steak",
+        imageUrl: require("./src/assets/images/steak.png")
+      },
+      {
+        name: "Pasta",
+        imageUrl: require("./src/assets/images/pasta.png")
+      },
+    ]
+
   return (
     <View style={styles.container}>
       <Header/>
       <Search/>
+
+        <FlatList data={commonCategories} renderItem={({item, index}) => {
+        return (
+        <commonCategories 
+          name={item.name} 
+          imageUrl={item.imageUrl} 
+          index={index}
+          />
+        )
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(category) => category.name}
+
+      />
+
       
-      <CategoryItem name="Burger" imageUrl={require('./src/assets/images/burger.png')}/>
-      <CategoryItem name="Pizza" imageUrl={require('./src/assets/images/pizza.png')}/>
-      <CategoryItem name="Dessert" imageUrl={require('./src/assets/images/dessert.png')}/>
       <StatusBar/>
       
     </View>
